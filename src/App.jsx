8710f7aef9f7ecd64428e444ef35230eb3eb777c
dangkien040308 <dangkien040308@gmail.com>
2024-03-03 14:43:30 +0700

    error again

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import Button from '@mui/material/Button'
 import { AccessAlarm, ThreeDRotation, Home } from '@mui/icons-material'
 import { pink } from '@mui/material/colors'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
 
 
   return (
     <>
+      <ModeToggle />
+      <hr/>
+      <div>KenIT</div>
       <Button variant="contained">Hello world</Button>
       <Button variant="text">Text</Button>
       <Button variant="contained">Contained</Button>
---
+++ src/theme.jsx
@@ -0,0 +1,23 @@
+import { deepOrange, orange, teal, cyan } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+
+// Create a theme instance.
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary : teal,
+        secondary : deepOrange
+      }
+    },
+    dark: {
+      palette: {
+        primary : cyan,
+        secondary : orange
+      }
+    }
+  }
+})
+
+export default theme
